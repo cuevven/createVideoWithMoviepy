ecf25f37aeb76b0dded33e8c7ce41567012b5381
Cuevven <cuevven@gmail.com>
2019-07-22 16:54:46 +0800

    Initial commit

---
+++ createVideoWithMoviepy.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+print('>>>>>>>>>>>>>>开始加载库>>>>>>>>>>>>>>')
+import os
+import math
+import librosa
+from moviepy.editor import TextClip, CompositeVideoClip, AudioFileClip, ImageSequenceClip, ImageClip
+import moviepy.video.fx.all as vfx
+import click
+from utils import resizeImage, readDir
+
+
+"""
+对于默认的 16:9 宽高比，请按以下分辨率进行编码：
+2160p：3840x2160
+1440p：2560x1440
+1080p：1920x1080 30fps/60fps 4000k-6000k/6000k-9000k
+720p：1280x720
+480p：854x480
+360p：640x360
+240p：426x240
+
+:param width: 生成视频的宽度 default: 1920 
+:param height: 生成视频的高度 default: 1080
+:param images_origin: 源图片的路径
+:param origin_target_dir: 将源路径替换为目标路径。
+    这个项目的源路径是爬虫下载图片的路径，最后还要把下载的图片处理成和视频一样的大小存放在目标目录
+    default: ('./src/images', './src/imgs')
+:param music: 背景音乐路径
+:param fps: 视频的帧率 default: 30
+:param output: 生成视频的文件名
+"""
+
+
+@click.command()
+@click.option('--width', prompt='Width', default=1920, help='The width of video clips')
+@click.option('--height', prompt='Height', default=1080, help='The height of video clips')
+@click.option('--images_origin', prompt='images file', default='./src/images/full/1353112775/2013', help='The source images path')
+@click.option('--origin_target_dir', prompt='replace origin dir to target dir', default=('./src/images', './src/imgs'), help='how replace origin dir to target dir')
+@click.option('--music', prompt='Music file', default='./src/music/1302.彩虹.mp3', help='The music file')
+@click.option('--fps', prompt='video fps ', default=30, help='The output video fps')
+@click.option('--output', prompt='Output file', default='./dist/1353112775.mp4', help='The output file name')
+def main(width, height, images_origin, origin_target_dir, music, fps, output):
+    target_images_dir = images_origin.replace(origin_target_dir[0], origin_target_dir[-1])
+    if not os.path.exists(target_images_dir):
+        filesPath = readDir(images_origin)
+        print('>>已载入文件 %s 个' % len(filesPath))
+        print('>>>>>>>>>>>>>>开始调整图片大小>>>>>>>>>>>>>>')
+        for img in filesPath:
+            resizeImage(img, origin_target_dir, (width, height))
+
+    print('>>>>>>>>>>>>>>开始读取调整过的图片>>>>>>>>>>>>>>')
+    filesPath = readDir(images_origin.replace(origin_target_dir[0], origin_target_dir[-1]))
+    print('>>已载入文件 %s 个' % len(filesPath))
+
+    print('>>>>>>>>>>>>>>开始分析节拍>>>>>>>>>>>>>>')
+    y, sr = librosa.load(music, sr=None)
+    tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+    beat_times = list(librosa.frames_to_time(beats, sr=sr))
+    beat_times.append(beat_times[-1] + 1)
+    librosa.onset.onset_detect
+    librosa.onset.onset_strength
+
+
+    clips = []
+    audio_time = librosa.get_duration(filename=music)
+    print('>>音频时长(s):%f >>节拍数量：%s' % (audio_time, len(beat_times)))
+    interval = math.ceil(abs( len(beat_times) / len(filesPath) ))
+    filesPath = [f for f in filesPath for i in range( interval )]
+    print('>>新的文件列表长度:%s' % len(filesPath))
+
+    print('>>>>>>>>>>>>>>开始按节拍生成视频帧>>>>>>>>>>>>>>')
+    for index, beat_time in enumerate(beat_times[:-1]):
+        if index >= len( filesPath):
+            print('>>图片数量不足以匹配节拍，中止匹配。输出的视频后段可能会出现黑屏。')
+            print('>>图片数量：{0} >节拍数量：{1}'.format(len( filesPath), len(beat_times)))
+            break
+        print(f'{index + 1}/{len(beat_times)}>>{ filesPath[index]}')
+        time_diff = math.modf(beat_time - beat_times[index -1])
+        time_diff = math.ceil(time_diff[0]*10) if (time_diff[0] * 10) > time_diff[-1] else math.ceil(time_diff[-1])
+        image_clip = (ImageClip(filesPath[index], duration=abs(time_diff)*fps)
+                    .set_fps(abs(time_diff)*fps)
+                    .set_start(beat_time)
+                    .set_end(beat_times[index + 1]))
+        image_clip = image_clip.set_pos('center')
+        if index % interval == 0:
+            image_clip = image_clip.fx(vfx.fadein, duration=0.5)
+        clips.append(image_clip)
+
+
+    print('>>>>>>>>>>>>>>开始合并剪辑，生成视频>>>>>>>>>>>>>>')
+    final_clip = CompositeVideoClip(clips)
+    audio_clip = AudioFileClip(music)
+    final_video = final_clip.set_audio(audio_clip)
+    final_video.write_videofile(
+        output,
+        fps=fps,
+        codec='mpeg4',
+        # ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow, placebo
+        preset='medium',
+        audio_codec="libmp3lame",
+        threads=4,
+        bitrate ='6000k')
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+import os
+from PIL import Image
+
+def resizeImage(target_image_path, origin_target_dir, target_size):
+    """
+    调整图片大小，缺失的部分用黑色填充
+    :param target_image_path: 图片路径
+    :param origin_target_dir: 图片最终保存目录 :type (origin_dir, target_dir)
+    :param target_size: 分辨率大小 :type (width, height)
+    :return:
+    """
+    image = Image.open(target_image_path)
+
+    iw, ih = image.size  # 原始图像的尺寸
+    w, h = target_size  # 目标图像的尺寸
+    scale = min(w / iw, h / ih)  # 转换的最小比例
+
+    # 保证长或宽，至少一个符合目标图像的尺寸
+    nw = int(iw * scale)
+    nh = int(ih * scale)
+
+    image = image.resize((nw, nh), Image.BICUBIC)  # 缩小图像
+    # image.show()
+
+    new_image = Image.new('RGB', target_size, (0, 0, 0, 0))  # 生成黑色图像
+    # // 为整数除法，计算图像的位置
+    new_image.paste(image, ((w - nw) // 2, (h - nh) // 2))  # 将图像填充为中间图像，两侧为灰色的样式
+    # new_image.show()
+
+    # 写入目标位置
+    target_image_path = target_image_path.replace(origin_target_dir[0], origin_target_dir[-1])
+    print('>>新图片：%s' % target_image_path)
+    target_dir = '/'.join(target_image_path.split('/')[:-1])
+    mkdirs(target_dir)
+    
+    new_image.save(target_image_path, quality=100)
+
+def mkdirs(path):
+    path=path.strip()
+    # 去除尾部 \ 符号
+    path=path.rstrip("\\")
+    # 判断路径是否存在
+    isExists=os.path.exists(path)
+    if not isExists:
+        # 如果不存在则创建目录,创建目录操作函数
+        '''
+        os.mkdir(path)与os.makedirs(path)的区别是:
+        当父目录不存在的时候os.mkdir(path)不会创建；
+        os.makedirs(path)则会创建父目录
+        '''
+        os.makedirs(path)
+        print('>>%s created.' % path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        print('>>%s is exists.' % path)
+        return False
+
+
+def readDir(dirPath):
+    if dirPath[-1] == '/':
+        print(u'>>文件夹路径末尾不能加/')
+        return
+    allFiles = []
+    if os.path.isdir(dirPath):
+        fileList = os.listdir(dirPath)
+        for f in fileList:
+            if f == '.DS_Store':
+                continue
+            
+            f = dirPath+'/'+f
+            if os.path.isdir(f):
+                subFiles = readDir(f)
+                allFiles = subFiles + allFiles #合并当前目录与子目录的所有文件路径
+            else:
+                allFiles.append(f)
+        return allFiles
+    else:
+        return 'Error,not a dir'
+
+def writeToFile(filesPath):
+    print('>>开始写入文件路径')
+    with open('GetAbsolutePath.txt', 'w') as f:
+        for filePath in filesPath:
+            # if filePath.split('/')[-1] != '.DS_Store':
+            f.write(str(filePath))
+            f.write('\n')
+    print('>>写入完毕，一共有 %s 行' % len(filesPath))
